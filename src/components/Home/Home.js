8986e5d0a4ac6cbda197ddabb25cbfabf7cdef53
Jesp <jstriker9peru@gmail.com>
2019-04-04 14:29:33 -0400

    Image and text

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -10,6 +10,16 @@ class Home extends Component {
         return (
             <div className="Home">
                 <Header links={this.state.links} />
+                <div className="first-container">
+                    <div className="front-text">
+                        <h1>Jonathan Espinoza</h1>
+                        <h2>Front-End Developer</h2>
+                        <p>I design and code beautifully simple things, and I love what I do.</p>
+                    </div>
+                    <div className="pic">
+                        <img src="https://previews.123rf.com/images/jemastock/jemastock1705/jemastock170506999/78062109-young-man-face-isolated-on-white-background-handsome-boy-face-expression-cartoon-vector-illustration.jpg" alt="avatar" />
+                    </div>
+                </div>
             </div>
         )
     }
