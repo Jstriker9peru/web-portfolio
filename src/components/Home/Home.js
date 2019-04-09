cce9e796b17dcf82ed4e6257bdf506cd0d31fa71
Jesp <jstriker9peru@gmail.com>
2019-04-09 08:52:50 -0400

    Added project hover state

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -131,30 +131,37 @@ class Home extends Component {
                 id: 'project1',
                 name: 'project1',
                 src: 'https://image.freepik.com/free-psd/modern-website-mockup_23-2148043716.jpg',
-                info: 'This is the way it goes'
+                info: 'This is the way it goes',
+                // hover: false
             },
             {
                 id: 'project2',
                 name: 'project2',
                 src: 'https://i0.wp.com/365webresources.com/wp-content/uploads/2018/10/FREE-Website-MOCKUP.jpg?ssl=1',
-                info: 'This is the way it shows'
+                info: 'This is the way it shows',
+                // hover: false
             },
             {
                 id: 'project3',
                 name: 'project3',
                 src: 'https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto/gigs/119072809/original/cc78b14638c8cae97938cd5839e5be74cf8394b2/design-creative-unique-psd-website-mockup-and-app-mockup-in-24-hours.png',
-                info: 'This is the way it mows'
+                info: 'This is the way it mows',
+                // hover: false
             },
             {
                 id: 'project4',
                 name: 'project4',
                 src: 'http://mockuplove.com/wp-content/uploads/2017/05/freshy-free-website-template.png',
-                info: 'This is the way it blows'
+                info: 'This is the way it blows',
+                // hover: false
             }
         ]
     
     }
+
+
     render() {
+
         return (
             <div className="Home">
                 <Header links={this.state.links} />
@@ -165,13 +172,13 @@ class Home extends Component {
                         <p>I design and code beautifully simple things, and I love what I do.</p>
                     </div>
                     <div className="pic">
-                        <img src="https://previews.123rf.com/images/jemastock/jemastock1705/jemastock170506999/78062109-young-man-face-isolated-on-white-background-handsome-boy-face-expression-cartoon-vector-illustration.jpg" alt="avatar" />
+                    <img src="https://previews.123rf.com/images/jemastock/jemastock1705/jemastock170506999/78062109-young-man-face-isolated-on-white-background-handsome-boy-face-expression-cartoon-vector-illustration.jpg" alt="avatar" />
                     </div>
                     <div className="resume-container">
                         <h3>Check Out My</h3>
                         <div className="button">
                             <div>Resume</div>
-                            <i class="fas fa-external-link-alt"></i>
+                            <i className="fas fa-external-link-alt"></i>
                         </div>
                     </div>
                 </section>
--- src/components/elements/Project/Project.js
+++ src/components/elements/Project/Project.js
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Project.scss';
 
-const Project = ({ name, projectId, projectSrc }) => {
-    return (
-        <div id={projectId} className="project-preview">
-            <img src={projectSrc} alt="mockup" width="450" height="250" />
-            <div className="project-hover"><h3>{name}</h3></div>
-        </div>
-    )
+class Project extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            hover: false,
+        }
+        this.onMouseOver = this.onMouseOver.bind(this);
+    }
+
+    onMouseOver(e) {
+        this.setState(prevState => {
+            return {
+                hover: !prevState.hover
+            }
+        })
+    }
+    render() {
+        const { name, projectId, projectSrc } = this.props;
+        return (
+            <div id={projectId} className={`project-preview ${this.state.hover ? 'hover-effect' : ''}`} onMouseEnter={this.onMouseOver} onMouseLeave={this.onMouseOver} >
+                <img src={projectSrc} alt="mockup" width="450" height="250" />
+                <div className="project-hover"><h3>{name}</h3></div>
+            </div>
+        )
+    }
 }
 
 export default Project;
