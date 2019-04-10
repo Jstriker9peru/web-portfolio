71028d3ce653e6e1816dcf954e8a32c5005a1736
Jesp <jstriker9peru@gmail.com>
2019-04-10 10:03:44 -0400

    SingleProject layout

--- src/components/SingleProject/SingleProject.js
+++ src/components/SingleProject/SingleProject.js
@@ -4,7 +4,25 @@ import './SingleProject.scss';
 const SingleProject = ({ match }) => {
     return(
         <div className="single-project-container">
-            <h1>Hello this is {match.params.projectId}</h1>
+            <div className="title-container">
+                <h1>{match.params.projectId}</h1>
+            </div>
+            <div className="top-container">
+                <div className="img-container">
+                    <img src="https://image.freepik.com/free-psd/modern-website-mockup_23-2148043716.jpg" alt="project-img" />
+                </div>
+                <div className="description-container">
+                    <h2>Project Details</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Id velit ut tortor pretium viverra suspendisse potenti nullam ac. Iaculis nunc sed augue lacus. Sapien faucibus et molestie ac feugiat sed lectus vestibulum. Cras tincidunt lobortis feugiat vivamus. Imperdiet nulla malesuada pellentesque elit. Eleifend donec pretium vulputate sapien nec sagittis aliquam malesuada. Faucibus interdum posuere lorem ipsum dolor. </p>
+                </div>
+            </div>
+            <div className="middle-container">
+                <div>
+                    <div>Technology Used</div>
+                    <div>Website Type</div>
+                    <div>Mobile Responsive</div>
+                </div>
+            </div>
         </div>
     )
 }
